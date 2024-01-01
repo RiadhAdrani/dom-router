950d55b480bf1e43ec7dabf1247c5bfb43068d47
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-01-02 00:08:48 +0100

    feat: flatten routes

--- src/class.v2.ts
+++ src/class.v2.ts
@@ -6,12 +6,10 @@ export interface PathRawRoute<T = unknown> {
   children?: Array<RawRoute<T>>;
 }
 
-export interface WrapperRawRoute<T = unknown> {
+export interface LayoutRawRoute<T = unknown> {
   element: T;
   children?: Array<RawRoute<T>>;
 }
-
-export type LayoutRawRoute<T = unknown> = PathRawRoute<T> | WrapperRawRoute<T>;
 
 export interface IndexRawRoute<T = unknown> {
   path: '';
@@ -26,7 +24,11 @@ export interface CatchRawRoute<T = unknown> {
   element?: T;
 }
 
-export type RawRoute<T = unknown> = LayoutRawRoute<T> | IndexRawRoute<T> | CatchRawRoute<T>;
+export type RawRoute<T = unknown> =
+  | LayoutRawRoute<T>
+  | IndexRawRoute<T>
+  | CatchRawRoute<T>
+  | PathRawRoute<T>;
 
 export interface NamedDestinationRequest {
   name: string;
@@ -57,26 +59,27 @@ export enum RouteType {
 
 export interface Route<T = unknown> {
   path: string;
-  steps: Array<T>;
+  steps: Array<T | undefined>;
   params: Array<string>;
   name?: string;
+  title?: string;
 }
 
-export const isLayoutRawRoute = (raw: RawRoute): raw is LayoutRawRoute => {
-  return 'path' in raw === false;
+export const isLayoutRawRoute = <T>(raw: RawRoute<T>): raw is LayoutRawRoute<T> => {
+  return !('path' in raw);
 };
 
-export const isIndexRawRoute = (raw: RawRoute): raw is IndexRawRoute => {
+export const isIndexRawRoute = <T>(raw: RawRoute<T>): raw is IndexRawRoute<T> => {
   return 'path' in raw && raw.path === '';
 };
 
-export const isCatchRawRoute = (raw: RawRoute): raw is CatchRawRoute => {
+export const isCatchRawRoute = <T>(raw: RawRoute<T>): raw is CatchRawRoute<T> => {
   return 'path' in raw && raw.path === '*';
 };
 
 export const joinPaths = (p1: string, p2: string): string => {
   if (p1.endsWith('/') && p2.startsWith('/')) {
-    return `${p1}${p1.substring(1)}`;
+    return `${p1}${p2.substring(1)}`;
   }
 
   return `${p1}${p2}`;
@@ -85,36 +88,89 @@ export const joinPaths = (p1: string, p2: string): string => {
 export const flattenRoutes = <T = unknown>(
   routes: Array<RawRoute<T>>,
   prevPath: string = '',
-  prevSteps: Array<T> = [],
+  prevSteps: Array<T | undefined> = [],
   prevParams: Array<string> = [],
 ): Record<string, Route<T>> => {
   return routes.reduce(
     (acc, it) => {
-      if (isCatchRawRoute(it)) {
-        const path = joinPaths(prevPath, it.path);
+      if (isIndexRawRoute<T>(it)) return acc;
 
+      if (isLayoutRawRoute<T>(it)) {
+        const steps = [...prevSteps, it.element];
+
+        if (it.children) {
+          const record = flattenRoutes<T>(it.children, prevPath, steps, prevParams);
+
+          return { ...acc, ...record };
+        }
+
+        return acc;
+      }
+
+      const path = joinPaths(prevPath, it.path);
+
+      // ignore index route
+      if (isCatchRawRoute<T>(it)) {
         const route: Route<T> = {
           params: prevParams,
-          path: joinPaths(path, it.path),
+          path,
           steps: [...prevSteps],
         };
 
-        if (it.element) {
-          route.steps.push(it.element);
-        }
+        route.steps.push(it.element);
 
         acc[path] = route;
 
         return acc;
       }
 
-      if (isLayoutRawRoute(it)) {
-        const steps = [...prevSteps, it.element];
+      // ts -_-
+      const raw = it as PathRawRoute<T>;
 
-        if (it.children) {
-          const record = flattenRoutes(it.children, prevPath);
+      // path route
+      const steps = [...prevSteps, raw.element];
+
+      // check for params
+      const params = [
+        ...prevParams,
+        ...raw.path.split('/').reduce((acc, it) => {
+          if (it.startsWith(':')) {
+            acc.push(it.substring(1));
+          }
+
+          return acc;
+        }, [] as Array<string>),
+      ];
+
+      const route: Route<T> = {
+        params,
+        path,
+        steps,
+        name: raw.name,
+      };
+
+      if (raw.children) {
+        const children = flattenRoutes(
+          raw.children,
+          route.path,
+          [...route.steps],
+          [...route.params],
+        );
+
+        acc = { ...acc, ...children };
+
+        // check if we have an index
+        const index = raw.children.find(it => isIndexRawRoute<T>(it)) as IndexRawRoute<T>;
+
+        if (index) {
+          route.steps.push(index.element);
+
+          route.name = index.name ?? route.name;
+          route.title = index.title ?? route.title;
         }
       }
+
+      acc[path] = route;
 
       return acc;
     },
